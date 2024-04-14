fb668b9aa20ac6a460e330bfe10402cb3b464af7
renankanu <renankanu@gmail.com>
2024-04-14 12:20:13 -0300

    feat: removed scroll down animation

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -4,66 +4,6 @@ import IntroTechStack from './IntroTechStack'
 import IntroTitle from './IntroTitle'
 
 export default function Intro() {
-  const css = `
-    .container {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-
-    .field {
-      display: flex;
-      align-items: center;
-      justify-content: space-around;
-      width: 100px;
-    }
-
-    .scroll {
-      width: 30px;
-      height: 30px;
-      border: 2px solid #FFF;
-      border-radius: 50%;
-      position: relative;
-      animation: down 1.5s infinite;
-      -webkit-animation: down 1.5s infinite;
-      &::before {
-        content: '';
-        position: absolute;
-        top: 7.5px;
-        left: 9px;
-        width: 9px;
-        height: 9px;
-        border-left: 2px solid #FFF;
-        border-bottom: 2px solid #FFF;
-        transform: rotate(-45deg);
-      }
-    }
-
-    @keyframes down {
-      0% {
-        transform: translate(0);
-      }
-      20% {
-        transform: translateY(15px);
-      }
-      40% {
-        transform: translate(0);
-      }
-    }
-
-    @-webkit-keyframes down {
-      0% {
-        transform: translate(0);
-      }
-      20% {
-        transform: translateY(15px);
-      }
-      40% {
-        transform: translate(0);
-      }
-    }
-  `
-
   return (
     <header className="background-grid background-grid--fade-out pb-20 pt-36">
       <div className="content-wrapper">
@@ -74,14 +14,6 @@ export default function Intro() {
           <div className="flex flex-col content-center">
             <IntroTitle />
             <IntroSocialMedia />
-            <div className="mt-10">
-              <style>{css}</style>
-              <div className="container">
-                <div className="field">
-                  <div className="scroll"></div>
-                </div>
-              </div>
-            </div>
             <IntroTechStack />
           </div>
           <div className="flex flex-row justify-center sm:hidden md:hidden">
